4af25aae456375b7c0598a1783f1417c317111f8
jbezanilla <joaquinbezanilla@gmail.com>
2023-01-30 19:35:13 +0100

    added first constructors and fixed details in essential files

--- src/characters.ts
+++ src/characters.ts
@@ -1,1 +1,44 @@
-export const n1 = 23;
+export class Character {
+  constructor(
+    public name: string,
+    public family: string,
+    public age: number,
+    public lifestate: boolean
+  ) {
+    this.lifestate = true;
+    this.name = name;
+    this.age = age;
+    this.family = family;
+  }
+  greetings() => {
+    console.log(`Hola soy ${this.name} y tengo ${this.age}`); // Cambiar de clg a "asignar string"
+  }
+  kill() {
+    this.lifestate = false;
+  }
+}
+
+class King extends Character {
+  constructor(
+    public name: string,
+    public family: string,
+    public age: number,
+    public lifestate: boolean,
+    public reigningAge: number
+  ) {
+    super(name, family, age, lifestate);
+  }
+  super.greetings() {
+    console.log("Vais a morir todos")
+  }
+}
+
+class Fighter extends Character {
+  constructor(
+    public name: string,
+    public family: string,
+    public age: number,
+    public lifestate: boolean,
+    public reigningAge: number
+  )
+}
